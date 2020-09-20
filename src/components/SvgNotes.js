9716e49f03f9c85783cc4c9aba783156b824e990
barbeque-squared <barbeque.squared@gmail.com>
2020-09-20 17:37:55 +0200

    render golden notes

--- src/components/SvgNotes.js
+++ src/components/SvgNotes.js
@@ -20,8 +20,13 @@ class SvgNotes extends Component {
     let baseColor = getColor(this.props.color).rgb()
     return (
       <>
+        <pattern id="golden" width={this.props.height} height={this.props.height} patternTransform="rotate(45 0 0)" patternUnits="userSpaceOnUse">
+          <line x1={0.5*this.props.height} y1="0" x2="0" y2="0" style={{stroke:'yellow', strokeWidth:1}} />
+        </pattern>
+        
         {this.props.notes.filter(this.isSingable).map((note, index) => (
           // TODO: y and height depend on difficulty (0.2 = Medium?)
+          // TODO: overlay a golden rect instead of trying to pattern it?
           <rect
             key={index}
             x={note.Start}
@@ -30,7 +35,7 @@ class SvgNotes extends Component {
             width={note.Length}
             height={2*this.props.height}
             className={this.noteTypeToClass(note.NoteType)}
-            fill={baseColor.lighten(0.8).string()}
+            fill={note.NoteType === NoteType.GOLDEN ? 'url(#golden)' : baseColor.lighten(0.8).string()}
             stroke={baseColor.lighten(0.5).string()}
             strokeWidth={0.2}
           />

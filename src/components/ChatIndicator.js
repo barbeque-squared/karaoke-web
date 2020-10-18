4d1a32e3791c9cd5d2e4dd529a45d7b542f42923
barbeque-squared <barbeque.squared@gmail.com>
2020-10-18 14:47:36 +0200

    better icon rendering

--- src/components/ChatIndicator.js
+++ src/components/ChatIndicator.js
@@ -10,7 +10,7 @@ class ChatIndicator extends Component {
         className="chat"
         title="When this icon is visible, you can voice-chat with other players. You will hear other players, and they will hear you. When the icon disappears, it's time to sing!"
       >
-        <svg viewBox="0 0 1024 896" fill="#4d4d4d">
+        <svg viewBox="0 0 1024 896" fill="#ffff">
           <path d="M256 512c0-64 0-192 0-192s-160 0-192 0-64 32-64 64 0 288 0 320 32 64 64 64 64 0 64 0v192l194-192s162 0 192 0 62-32 62-64 0-64 0-64-128 0-192 0-128-64-128-128z m576-384c-32 0-416 0-448 0s-64 32-64 64 0 288 0 320 32 64 64 64 190 0 190 0l194 192v-192s32 0 64 0 64-32 64-64 0-288 0-320-32-64-64-64z" />
         </svg>
       </div>

c8261301e8cebbbd58fdf8db0fcb5aeee00a7b4a
barbeque-squared <barbeque.squared@gmail.com>
2020-10-23 16:02:06 +0200

    icon visibility

--- src/components/LiveScoreIndicator.js
+++ src/components/LiveScoreIndicator.js
@@ -10,7 +10,7 @@ class LiveScoreIndicator extends Component {
     return (
       <div className="livescore" title="Spectator mode: due to rate limits, you will not see the notes as they are sung by players (but you will still see the notes they are supposed to sing, current scores, and everything else). Contact the game host if you are supposed to be singing.">
         <svg viewBox="0 0 22 22">
-          <g transform="matrix(.02146 0 0 .02146 1 1)" fill="#4d4d4d">
+          <g transform="matrix(.02146 0 0 .02146 1 1)" fill="#ffffff">
             <path d="m466.07 161.53c-205.6 0-382.8 121.2-464.2 296.1-2.5 5.3-2.5 11.5 0 16.9 81.4 174.9 258.6 296.1 464.2 296.1 205.6 0 382.8-121.2 464.2-296.1 2.5-5.3 2.5-11.5 0-16.9-81.4-174.9-258.6-296.1-464.2-296.1m0 514.7c-116.1 0-210.1-94.1-210.1-210.1 0-116.1 94.1-210.1 210.1-210.1 116.1 0 210.1 94.1 210.1 210.1 0 116-94.1 210.1-210.1 210.1"/>
             <circle cx="466.08" cy="466.02" r="134.5"/>
           </g>

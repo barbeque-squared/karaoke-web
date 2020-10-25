9bafd756d8e353f3040c899e4d08a7c39004d1bd
barbeque-squared <barbeque.squared@gmail.com>
2020-10-25 13:17:12 +0100

    use websockets instead of pusher/ably

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import '../App.css';
 import * as firebase from 'firebase/app';
 import 'firebase/database';
-//~ import Pusher from 'pusher-js';
 
 import SingScreen from './SingScreen';
 import LiveScoreStatus from '../constants/LiveScoreStatus';
@@ -32,7 +31,7 @@ class App extends Component {
       sentenceinfo: {sentences: [], startbeat: 0, totalbeats: 0},
       songlist: [],
       playernotes: [],
-      livescorestatus: LiveScoreStatus.CONNECTING,
+      livescorestatus: LiveScoreStatus.CONNECTED,
       microphone: true,
     }
   }
@@ -53,44 +52,16 @@ class App extends Component {
       let database = firebase.database();
       // setup handlers
       database.ref('karaoke/background').on('value', (d) => {this.setState({background: d.val()})});
-      database.ref('karaoke/microphone').on('value', d => {this.setState({microphone: d.val()})});
-      database.ref('karaoke/colors').on('value', (d) => {this.setState({colors: d.val()})});
-      database.ref('karaoke/currentbeat').on('value', (d) => {this.setState({currentbeat: d.val()})});
-      database.ref('karaoke/levels').on('value', (d) => {this.setState({levels: d.val()})});
-      database.ref('karaoke/livecode').once('value', (d) => {this.initPusher(d.val())})
-      database.ref('karaoke/names').on('value', (d) => {this.setState({names: d.val()})});
-      database.ref('karaoke/notes1').on('value', (d) => {this.setState({notes1: d.val()})});
-      database.ref('karaoke/notes2').on('value', (d) => {this.setState({notes2: d.val()})});
-      database.ref('karaoke/scores').on('value', (d) => {this.setState({scores: d.val()})});
-      database.ref('karaoke/sentenceinfo').on('value', (d) => {this.setState({sentenceinfo: d.val()})});
       database.ref('karaoke/songlist').once('value', (d) => {this.setState({songlist: d.val()})});
+      database.ref('karaoke/websocket').once('value', (d) => {this.initSocket(d.val())})
     } catch (e) {
       console.error(e);
     }
-    
   }
   
-  initPusher(livecode) {
-    if (process.env.NODE_ENV !== 'development' && document.location.hash !== livecode) {
-      this.setState({livescorestatus: LiveScoreStatus.NOT_CONNECTED})
-      return
-    }
-    
-    // PUSHER CODE
-    //~ const pusher = new Pusher(process.env.REACT_APP_PUSHER_KEY, {
-      //~ cluster: process.env.REACT_APP_PUSHER_CLUSTER,
-    //~ })
-    //~ const channel = pusher.subscribe('karaoke')
-    //~ channel.bind('playernotes', (data) => {
-      //~ this.setState({playernotes: data});
-    //~ })
-    
-    // ABLY CODE
-    const ably = new require('ably').Realtime(process.env.REACT_APP_ABLY_KEY)
-    const channel = ably.channels.get('karaoke')
-    channel.subscribe('sing', (message) => {this.setState({playernotes: message.data})})
-
-    this.setState({livescorestatus: LiveScoreStatus.CONNECTED})
+  initSocket(url) {
+    const socket = new WebSocket(url)
+    socket.addEventListener('message', (event) => {this.setState(JSON.parse(event.data))})
   }
 }
 

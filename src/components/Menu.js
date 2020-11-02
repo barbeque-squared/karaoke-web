f3e2672612bae230d64c9cd5a5dd2a703173e016
barbeque-squared <barbeque.squared@gmail.com>
2020-11-02 13:16:49 +0100

    remove songbrowser from menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,8 +3,6 @@ import Modal from 'react-modal'
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
 import 'react-tabs/style/react-tabs.css'
 
-import SongBrowser from './SongBrowser'
-
 Modal.setAppElement('body')
 
 class Menu extends PureComponent {
@@ -34,12 +32,10 @@ class Menu extends PureComponent {
         <Modal isOpen={this.state.show} onRequestClose={this.hide.bind(this)} contentLabel="Menu">
           <Tabs>
             <TabList>
-              <Tab>Songs</Tab>
               <Tab>Settings</Tab>
               <Tab>About</Tab>
               <button className="close" title="Close" onClick={this.hide.bind(this)}>X</button>
             </TabList>
-            <TabPanel><SongBrowser songs={this.props.songs} /></TabPanel>
             <TabPanel>TODO</TabPanel>
             <TabPanel>TODO</TabPanel>
           </Tabs>

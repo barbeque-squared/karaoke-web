e136adbe5266df422edb808715bb7e97ebe467aa
barbeque-squared <barbeque.squared@gmail.com>
2020-12-12 12:40:20 +0100

    more temporary hacks

--- src/components/schedule/ScheduleBanner.js
+++ src/components/schedule/ScheduleBanner.js
@@ -22,7 +22,10 @@ class ScheduleBanner extends PureComponent {
     return (
       <div className={'ScheduleBanner'}>
         <h1>Check the Discord text room for instructions!</h1>
-        <p>You need to install <a href="https://www.mumble.info/downloads">Mumble</a></p>
+        <p>
+          You need to install <a href="https://www.mumble.info/downloads">Mumble</a>.
+          Check the pinned message for details.
+        </p>
       </div>
     )
     //~ const sessions = this.props.schedule.filter(s => !this.ended(s))
